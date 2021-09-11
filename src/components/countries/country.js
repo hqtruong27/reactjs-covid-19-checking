c11aba12e188280120cb4785c4be235f00c1a3eb
Truong Hoang <hqtruong27@gmail.com>
2021-09-12 01:38:44 +0700

    component country

--- src/components/countries/country.js
+++ src/components/countries/country.js
@@ -1,14 +1,14 @@
 import { FormControl, FormHelperText, InputLabel, NativeSelect } from '@material-ui/core';
 import React from 'react'
 
-const Country = ({ value, event, countries }) => {
+const Country = ({ value, onChange, data }) => {
 
     return (
         <FormControl>
             <InputLabel htmlFor="country" shrink >Countries</InputLabel>
-            <NativeSelect value={value} onChange={event} inputProps={{ id: 'country', name: 'countries' }}>
+            <NativeSelect value={value} onChange={onChange} inputProps={{ id: 'country', name: 'countries' }}>
                 {
-                    countries.map((x) => { return <option value={x.ISO2}>{x.Country}</option> })
+                    data.map((x) => <option key={x.ISO2} value={x.slug}>{x.Country}</option>)
                 }
             </NativeSelect>
             <FormHelperText>Choose your country</FormHelperText>
