a36fd61867371484604896a05161d10f22734131
Truong Hoang <hqtruong27@gmail.com>
2021-09-12 01:39:09 +0700

    highlight, sum  cpn

--- src/components/highlight/highlight.js
+++ src/components/highlight/highlight.js
@@ -1,18 +1,72 @@
-import { Grid } from '@material-ui/core'
+import { Card, CardContent, Grid, Typography, makeStyles } from '@material-ui/core'
 import React from 'react'
+import { TYPE } from '../../global/constants'
 
-const Highlight = () => {
+
+export const Highlight = ({ report }) => {
+    const data = report[report.length - 1] ?? []
+
+    const summary = [
+        {
+            title: 'Number of cases of covid',
+            total: data.Confirmed ?? 'NA',
+            type: TYPE.CONFIRMED
+        },
+        {
+            title: 'Recovered',
+            total: data.Recovered ?? 'NA',
+            type: TYPE.RECOVERED
+        },
+        {
+            title: 'Deaths',
+            total: data.Deaths ?? 'NA',
+            type: TYPE.DEATHS
+        },
+    ]
+
     return (
-        <Grid container spacing={3}>
-            <Grid item sm={8} xs={12}>
-
+        <div>
+            <Grid container spacing={3}>
+                {summary.map(item => <HighlightCardContent key={item.type} type={item.type} title={item.title} total={item.total} />)}
             </Grid>
-
-            <Grid item sm={4} xs={12}>
-
-            </Grid>
-       </Grid>
+        </div>
     )
 }
 
-export default Highlight
+const HighlightCardContent = ({ type, title, total }) => {
+    console.log(type, title, total)
+    const classes = styles({ type })
+    return (
+        <Grid item sm={4} xs={12} >
+            <Card >
+                <CardContent className={classes.wrapper} >
+                    <Typography className={classes.title} component='p'>{title}</Typography>
+                    <Typography className={classes.total} component='p'>{total}</Typography>
+                </CardContent>
+            </Card>
+        </Grid>
+    )
+}
+
+const styles = makeStyles({
+    wrapper: ({ type }) => {
+        switch (type) {
+            case TYPE.CONFIRMED:
+                return { borderLeft: '6px solid #c9302c' }
+            case TYPE.RECOVERED:
+                return { borderLeft: '6px solid #927228' }
+            case TYPE.DEATHS:
+                return { borderLeft: '6px solid #333' }
+            default:
+                return { borderLeft: '6px solid #c9302c' };
+        }
+    },
+    title: {
+        fontSize: '18px',
+        marginBottom: '5px'
+    },
+    total: {
+        fontSize: '18px',
+        fontWeight: 'bold',
+    }
+})
--- src/components/summary/summary.js
+++ src/components/summary/summary.js
@@ -1,36 +1,17 @@
-import { Card, CardContent, Grid, Typography } from '@material-ui/core'
+import { Grid } from '@material-ui/core'
 import React from 'react'
 
 const Summary = () => {
     return (
-        <div>
-            <Grid container spacing={3}>
-                <Grid item sm={4} xs={12} >
-                    <Card>
-                        <CardContent>
-                            <Typography component='p'>Number of cases of covid</Typography>
-                            <Typography component='p'>2222</Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
-                <Grid item sm={4} xs={12} >
-                    <Card>
-                        <CardContent>
-                            <Typography component='p'>Number of cases of covid</Typography>
-                            <Typography component='p'>2222</Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
-                <Grid item sm={4} xs={12} >
-                    <Card>
-                        <CardContent>
-                            <Typography component='p'>Number of cases of covid</Typography>
-                            <Typography component='p'>222</Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
+        <Grid container spacing={3}>
+            <Grid item sm={8} xs={12}>
+
             </Grid>
-        </div>
+
+            <Grid item sm={4} xs={12}>
+
+            </Grid>
+        </Grid>
     )
 }
 
