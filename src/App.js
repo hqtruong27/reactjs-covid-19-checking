382fe34e9d9d092290af31447e714227c60eeb10
Truong Hoang <hqtruong27@gmail.com>
2021-09-12 16:30:39 +0700

    update app

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
 import { useEffect, useState } from "react";
 import CoronaAPI from "./components/api/corona-api";
-import Country from "./components/countries/country";
+import { Country } from "./components/countries/country";
 import { Highlight } from "./components/highlight/highlight";
 import Summary from "./components/summary/summary";
-
+import { COUNTRIES } from './global/constants'
 
 function App() {
 
+  const [open, setOpen] = useState(false);
   const [countries, setCountries] = useState([])
 
-  const [country, setCurrentCountry] = useState('')
+  const loading = open && countries.length === 0
+  const [country, setCurrentCountry] = useState({ Country: null, Slug: null })
 
   const [report, setReport] = useState([])
 
   useEffect(() => {
-    CoronaAPI.getCountries().then((res) => {
-      setCountries(res.data);
-    }).catch((err) => console.log(err))
+    setCurrentCountry({ Country: "Viet Nam", Slug: COUNTRIES.VN })
   }, [])
 
+  useEffect(() => {
+    if (!loading) return
 
-  const onChange = (event) => setCurrentCountry(event.target.value)
+    setTimeout(() => {
+      CoronaAPI.getCountries().then((res) => {
+        setCountries(res.data)
+      }).catch((err) => console.log(err))
+    }, 1e3)
+
+  }, [loading])
+
 
   useEffect(() => {
-    if (country) {
-      CoronaAPI.getReportByCountry(country).then((res) => {
+    if (!open) {
+      setCountries([]);
+    }
+  }, [open]);
+
+  const onChange = (event, value) => {
+    setCurrentCountry({ Slug: value.Slug, Country: value.Country })
+  }
+
+  useEffect(() => {
+    if (country?.Slug) {
+      CoronaAPI.getReportByCountry(country.Slug).then((res) => {
         setReport(res.data)
       }).catch((err) => console.error(err))
     }
@@ -32,7 +51,14 @@ function App() {
 
   return (
     <>
-      <Country data={countries} onChange={onChange} />
+      <Country
+        open={open}
+        onOpen={() => setOpen(true)}
+        onClose={() => setOpen(false)}
+        data={countries}
+        loading={loading}
+        onChange={onChange}
+        val={country} />
       <Highlight report={report} />
       <Summary report={report} />
     </>
