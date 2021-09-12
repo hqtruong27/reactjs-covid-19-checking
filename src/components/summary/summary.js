debf51f9833ff9cae4b2c181cc438b4d9794217b
Truong Hoang <hqtruong27@gmail.com>
2021-09-12 23:29:30 +0700

    update

--- src/components/summary/summary.js
+++ src/components/summary/summary.js
@@ -1,11 +1,12 @@
 import { Grid } from '@material-ui/core'
 import React from 'react'
+import HighChart from '../charts/high-charts'
 
-const Summary = () => {
+const Summary = ({ data }) => {
     return (
         <Grid container spacing={3}>
             <Grid item sm={8} xs={12}>
-
+                <HighChart data={data} />
             </Grid>
 
             <Grid item sm={4} xs={12}>
