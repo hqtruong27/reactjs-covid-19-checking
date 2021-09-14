cf1514d64d36824dc7763723ec950aa243e44bd8
Truong Hoang <hqtruong27@gmail.com>
2021-09-14 22:41:05 +0700

    new API

--- src/components/api/corona-api.js
+++ src/components/api/corona-api.js
@@ -1,20 +1,57 @@
 import axios from "axios"
+import moment from "moment"
 
-
-const CoronaAPI = {
-    getCountries: () => {
-        return axios.get('https://api.covid19api.com/countries')
-    },
-
-    getReportByCountry: (country) => {
-        const config = {
-            method: 'get',
-            url: `https://api.covid19api.com/dayone/country/${country}`,
-            headers: {}
-        };
-
-        return axios(config)
+let config = {
+    baseURL: 'https://api.covid19api.com',
+    headers: {
+        'Content-Type': 'application/json',
     }
 }
 
-export default CoronaAPI;
+const CoronaAPI = {
+    /**
+     * get all countries
+     * @returns
+     */
+    countries: () => axios.get('/countries', config),
+
+    /**
+     * get report by Country
+     * @param {*} country Slug country
+     * @returns
+     */
+    getReportByCountry: (country) => axios.get(`/dayone/country/${country}`, config),
+
+    /**
+     * sum report date now - 1 day
+     * @param {*} country slug country
+     * @returns
+     */
+    sumReportByCountry: (country) => axios.get(`/live/country/${country}/status/all/date/${moment().subtract(2, 'days').format('YYYY-MM-DD')}`, config),
+
+
+    /**
+     * get report by Country
+     * @param {*} country
+     * @param {*} from from date yyyy/mm/dd
+     * @param {*} to to date yyyy/mm/dd
+     * @returns
+     */
+    getReportPremiumByCountry: (country, from, to) => {
+
+        config.headers['X-Access-Token'] = '2f0d9db1-1d6c-4813-ac99-c19a8b6aedab'
+
+        let url = `/premium/country/${country}`
+
+        if (from) {
+            url += `?from=${from}`
+            if (to) url += `&to=${to}`
+        } else {
+            if (to) url += `?to=${to}`
+        }
+
+        return axios.get(url, config)
+    }
+}
+
+export default CoronaAPI
