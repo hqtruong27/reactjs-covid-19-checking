33cae6d6a5e7d247741be66ba356d0701a725c26
Truong Hoang <hqtruong27@gmail.com>
2021-09-18 02:42:28 +0700

    change autocomplete select to seach autoComplete

--- src/App.js
+++ src/App.js
@@ -1,42 +1,77 @@
-import { Backdrop, CircularProgress } from "@mui/material";
-import { useEffect, useState } from "react";
-import { Country } from "./components/countries/country";
-import { Highlight } from "./components/highlight/highlight";
-import { COUNTRIES } from './global/constants'
+import { Autocomplete } from "@material-ui/lab"
+import { makeStyles, TextField, InputAdornment, Grid } from "@material-ui/core"
+import { Backdrop, CircularProgress } from "@mui/material"
+import { useEffect, useState } from "react"
+import { createBrowserHistory } from 'history'
+import { Search } from "@material-ui/icons"
 
-import Summary from "./components/summary/summary";
-import CoronaAPI from "./components/api/corona-api";
+import { Highlight } from "./components/highlight/highlight"
+import { COUNTRIES } from './global/constants'
+import _context from './database/db-dexie'
+import Summary from "./components/summary/summary"
+import CoronaAPI from "./components/api/corona-api"
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "95%",
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(4),
+    '@media(min-width: 780px)': {
+      width: "45%",
+    },
+  },
+  inputRoot: {
+    borderRadius: 100,
+    border: '1px solid #dadce0',
+    boxShadow: '0 1px 5px rgb(0 0 0 / 25%)',
+    // "& .MuiOutlinedInput-notchedOutline": {
+    //   borderWidth: "1px",
+    //   borderColor: "#dadce0"
+    // },
+    "&:hover .MuiOutlinedInput-notchedOutline": {
+      borderWidth: "1px",
+      borderColor: "#dadce0"
+    },
+    // "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+    //   borderWidth: "1px",
+    //   borderColor: "#dadce0"
+    // }
+  }
+}))
+
+let currentValue;
+let history = createBrowserHistory()
 
 function App() {
-
+  const classes = useStyles()
   const [open, setOpen] = useState(false);
+  const [inputValue, setInputValue] = useState('');
   const [loadingBackDrop, setLoadingBackDrop] = useState(false);
   const [countries, setCountries] = useState([])
   const [params, setParams] = useState({ Country: null, Slug: null, from: null })
   const [report, setReport] = useState([])
-
-  const loading = open && countries.length === 0
 
   useEffect(() => {
     setParams(COUNTRIES.VIE)
   }, [])
 
   useEffect(() => {
-    if (!loading) return
-    setTimeout(() => {
-      CoronaAPI.countries().then((res) => {
-        setCountries(res.data)
-      }).catch((err) => console.log(err))
-    }, 1e3)
+    _context.table('countries').get('cached-country').then((response) => {
+      if (response) {
+        CoronaAPI.countries().then((res) => {
+          console.log(res)
+          setCountries(res.data)
+          _context.table('countries').put(res.data, 'cached-country').then()
+          return;
+        }).catch((err) => {
+          console.log(err)
+        })
+      }
 
-  }, [loading])
-
-
-  useEffect(() => {
-    if (!open) {
-      setCountries([]);
-    }
-  }, [open]);
+      console.log(countries)
+      return setCountries(response)
+    }).catch((err) => console.log(err))
+  }, [])
 
   const handleChangeCountry = (_, value) => {
     setParams({ Slug: value?.Slug, Country: value?.Country })
@@ -63,23 +98,83 @@ function App() {
 
   return (
     <div>
-      <Backdrop
-        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={loadingBackDrop}>
-        <CircularProgress color="inherit" />
-      </Backdrop>
-      <Country
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        justifyContent="center"  >
+        <Grid className={classes.root} >
+          <Autocomplete
+            disableClearable
+            classes={{ inputRoot: classes.inputRoot }}
+            freeSolo
+            inputValue={inputValue ?? ''}
+            onChange={(_, value) => {
+              setOpen(false)
+              setInputValue(value?.Country)
+            }}
+            onFocus={(e) => {
+              setInputValue('')
+              currentValue = e.target.value
+              console.log({ targetFocus: e.target.value, valueFocus: currentValue })
+            }}
+            onBlur={(e) => {
+              history.push({
+                pathname: '/home',
+                search: '?the=query'
+              })
+
+              console.log(params)
+              setOpen(false)
+              setInputValue(e.target.value !== '' ? e.target.value : currentValue)
+            }}
+            onInputChange={(event) => {
+              const value = event.target.value
+
+              setOpen(value?.length > 2 ? true : false)
+              console.log(value)
+              setInputValue(value)
+            }}
+            open={open}
+            options={countries}
+            getOptionLabel={(option) => option.Country || ''}
+            renderInput={(params) =>
+              <TextField
+                {...params}
+                label="Select a location"
+                variant="outlined"
+                fullWidth
+                InputProps={{
+                  ...params.InputProps,
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <Search />
+                    </InputAdornment>
+                  )
+                }}
+              />
+            }
+          />
+        </Grid>
+      </Grid>
+
+      {/* <Country
         open={open}
         onOpen={() => setOpen(true)}
         onClose={() => setOpen(false)}
         data={countries}
         loading={loading}
         onChange={handleChangeCountry}
-        val={params} />
+        val={params} /> */}
       <Highlight sum={report.data} />
       <Summary onChange={handleChangeSummary} data={report} />
 
-
+      {/* --backdrop loading-- */}
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={loadingBackDrop}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
     </div>
   );
 }
