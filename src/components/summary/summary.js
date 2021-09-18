fcd5ca590e230c376b543b941b27b87ef179a995
Truong Hoang <hqtruong27@gmail.com>
2021-09-19 01:34:01 +0700

    add default value

--- src/components/summary/summary.js
+++ src/components/summary/summary.js
@@ -2,11 +2,11 @@ import { Grid } from '@material-ui/core'
 import React from 'react'
 import HighChart from '../charts/high-charts'
 
-const Summary = ({ onChange, data }) => {
+const Summary = ({ value, onChange, data }) => {
     return (
         <Grid container spacing={3}>
             <Grid item sm={8} lg={8} xs={12} md={12} >
-                <HighChart onChange={onChange} data={data} />
+                <HighChart value={value} onChange={onChange} data={data} />
             </Grid>
 
             <Grid item sm={4} lg={8} xs={12} md={12} >
