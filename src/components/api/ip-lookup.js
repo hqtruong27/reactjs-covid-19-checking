a922180df6850d55af884a4faa6508c445e49ba3
Truong Hoang <hqtruong27@gmail.com>
2021-09-21 02:20:03 +0700

    up

---
+++ src/components/api/ip-lookup.js
@@ -0,0 +1,13 @@
+import axios from "axios"
+
+const IP = {
+    lookup: () => {
+        const config = {
+            baseURL: 'https://geolocation-db.com/'
+        }
+
+        return axios.get('json/', config)
+    }
+}
+
+export default IP
