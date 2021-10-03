f422ab3cfde6eff85ef26a0a37bc434f141225f3
Truong Hoang <hqtruong27@gmail.com>
2021-10-03 19:24:42 +0700

    refactor file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { TextField, InputAdornment, Grid, makeStyles } from '@material-ui/core'
+import { TextField, InputAdornment, Grid, makeStyles, Container } from '@material-ui/core'
 import { Backdrop, CircularProgress } from '@mui/material'
 import { Autocomplete } from '@material-ui/lab'
 import { useEffect, useState } from "react"
@@ -10,7 +10,7 @@ import UrlExtension from './global/extension'
 import _context from './database/db-dexie'
 import Summary from "./components/summary/summary"
 import CoronaAPI from "./components/api/corona-api"
-import IP from './components/api/ip-lookup'
+import data1  from './global/countries.json'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -46,8 +46,9 @@ function App() {
   const [inputValue, setInputValue] = useState('')
   const [loadingBackDrop, setLoadingBackDrop] = useState(false)
   const [countries, setCountries] = useState([])
-  const [params, setParams] = useState({ Country: null, Slug: null, from: null })
+  const [params, setParams] = useState({ Country: null, Slug: null, ISO2: null, from: null })
   const [report, setReport] = useState([])
+  const [summary, setSummary] = useState({})
 
   useEffect(() => {
     _context.table('countries').get('cached-country').then(async (response) => {
@@ -59,6 +60,7 @@ function App() {
           await Promise.all([init(data), _context.table('countries').put(data, 'cached-country')])
         }
       } else {
+        debugger
         setCountries(response)
         await init(response)
       }
@@ -66,121 +68,105 @@ function App() {
   }, [])
 
   const init = async (arr) => {
-    let code
-    const { hl, sum } = UrlExtension.find(['hl', 'sum'])
+    const summary = await CoronaAPI.summaryTotalAsync()
+    console.log(data1)
+    if (summary) {
+      const { hl, sum } = UrlExtension.find(['hl', 'sum'])
 
-    if (!hl) {
-      const response = await IP.lookup()
-      if (response) {
-        code = response.data?.country_code?.toLowerCase()
-      }
-    } else code = hl.toLowerCase()
-
-    const result = arr.find(x => x.Slug.toLowerCase() === code || x.ISO2.toLowerCase() === (code ?? COUNTRIES.VIE.Code))
-    setParams({ Country: result.Country, Slug: result.Slug, from: sum })
-    setInputValue(result?.Country)
+    }
   }
 
   const handleChangeCountry = (_, value) => {
     const { hl, sum } = UrlExtension.find(['hl', 'sum'])
     const slug = hl ? hl : value?.Slug
-    setParams({ Slug: slug, Country: value?.Country, from: sum ?? undefined })
+    setParams({ Slug: slug, Country: value?.Country, ISO2: value?.ISO2, from: sum ?? undefined })
   }
 
   const handleChangeSummary = (event) => {
     const from = event.target?.value
     UrlExtension.append('sum', from)
-    setParams({ Slug: params?.Slug, Country: params?.Country, from: from })
+    setParams({ Slug: params?.Slug, Country: params?.Country, ISO2: params?.ISO2, from: from })
   }
 
   useEffect(() => {
-    if (params?.Slug) {
+    if (params?.Slug && params?.ISO2) {
       setLoadingBackDrop(true)
       CoronaAPI.getReportPremiumByCountry(params.Slug, params.from).then((res) => {
         const result = { type: params.from, data: res.data }
-
         setReport(result)
-        setLoadingBackDrop(false)
-      }).catch((err) => {
-        console.error(err)
-        setLoadingBackDrop(false)
-      })
+        return CoronaAPI.specificCountry(params.ISO2)
+      }).then(response => {
+        if (response) {
+          setSummary(response.data)
+        }
+      }).catch((err) => console.error(err))
+        .finally(() => setLoadingBackDrop(false))
     }
   }, [params])
 
   return (
     <div>
-      <Grid
-        container
-        direction="column"
-        alignItems="center"
-        justifyContent="center"  >
-        <Grid className={classes.root} >
-          <Autocomplete
-            disableClearable
-            classes={{ inputRoot: classes.inputRoot }}
-            freeSolo
-            inputValue={inputValue ?? ''}
-            onChange={(_, value) => {
-              const slug = value?.Slug
-              if (slug) UrlExtension.append('hl', slug)
-
-              setOpen(false)
-              setInputValue(value?.Country)
-              handleChangeCountry(_, value)
-            }}
-            onFocus={(e) => {
-              setInputValue()
-            }}
-            onBlur={(e) => {
-              setOpen(false)
-              setInputValue(params.Country)
-            }}
-            onInputChange={(event) => {
-              const value = event?.target?.value
-              setOpen(value?.length > 2 ? true : false)
-              setInputValue(value)
-            }}
-            open={open}
-            options={countries}
-            getOptionLabel={(option) => option.Country || ''}
-            renderInput={(params) =>
-              <TextField
-                {...params}
-                placeholder="Select a location"
-                variant="outlined"
-                fullWidth
-                InputProps={{
-                  ...params.InputProps,
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <Search />
-                    </InputAdornment>
-                  )
-                }}
-              />
-            }
-          />
+      <Container maxWidth="xl">
+        <Grid container
+          direction="column"
+          alignItems="center"
+          justifyContent="center"  >
+          <Grid className={classes.root} >
+            <Autocomplete disableClearable
+              classes={{ inputRoot: classes.inputRoot }}
+              inputValue={inputValue ?? ''}
+              onChange={(_, value) => {
+                const slug = value?.Slug
+                if (slug) UrlExtension.append('hl', slug)
+                setOpen(false)
+                setInputValue(value?.Country)
+                handleChangeCountry(_, value)
+              }}
+              onFocus={(e) => {
+                setInputValue()
+              }}
+              onBlur={(e) => {
+                setOpen(false)
+                setInputValue(params.Country)
+              }}
+              onInputChange={(event) => {
+                const value = event?.target?.value
+                setOpen(value?.length > 2)
+                setInputValue(value)
+              }}
+              open={open}
+              options={countries}
+              getOptionLabel={(option) => option.Country || ''}
+              getOptionSelected={(option, value) => option.ISO2 === value.ISO2}
+              renderInput={(params) =>
+                <TextField
+                  {...params}
+                  placeholder="Select a location"
+                  variant="outlined"
+                  fullWidth
+                  InputProps={{
+                    ...params.InputProps,
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <Search />
+                      </InputAdornment>
+                    )
+                  }}
+                />
+              }
+            />
+          </Grid>
         </Grid>
-      </Grid>
+        <Highlight data={summary} />
+        <Summary value={params?.from} onChange={handleChangeSummary} data={report} />
 
-      {/* <Country
-        open={open}
-        onOpen={() => setOpen(true)}
-        onClose={() => setOpen(false)}
-        data={countries}
-        loading={loading}
-        onChange={handleChangeCountry}
-        val={params} /> */}
-      <Highlight sum={report.data} />
-      <Summary value={params?.from} onChange={handleChangeSummary} data={report} />
-
-      {/* --backdrop loading-- */}
-      <Backdrop
-        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={loadingBackDrop}>
-        <CircularProgress color="inherit" />
-      </Backdrop>
+        {/* --backdrop loading-- */}
+        <Backdrop
+          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={loadingBackDrop}>
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      </Container>
     </div>
   )
 }
