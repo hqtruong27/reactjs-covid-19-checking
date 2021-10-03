9ccb66f19f80ad6e022e754f8003f7e65abbbbf8
Truong Hoang <hqtruong27@gmail.com>
2021-10-03 19:24:05 +0700

    new api

--- src/components/api/corona-api.js
+++ src/components/api/corona-api.js
@@ -1,12 +1,33 @@
 import axios from 'axios'
 import dayjs from 'dayjs'
+import countriesFile from '../../global/countries.json'
+
+const utm_source = 'covid19checking.com'
 
 const config = {
     baseURL: 'https://api.covid19api.com',
     headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Request-From': 'https://covid19checking.com',
+    },
+    params: {
+        utm_source: utm_source
     }
 }
+
+const config_Novel = {
+    baseURL: 'https://disease.sh/v3/covid-19/',
+    headers: {
+        'Content-Type': 'application/json',
+        'Request-From': 'https://covid19checking.com',
+    },
+    params: {
+        t: new Date().getTime(),
+        utm_source: utm_source
+    }
+}
+
+const url_oWid_vaccine = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/latest/owid-covid-latest.json'
 
 const CoronaAPI = {
     /**
@@ -15,6 +36,7 @@ const CoronaAPI = {
      */
     countries: async () => await axios.get('/countries', config),
 
+    countriesAsync: () => countriesFile,
     /**
      * get report by Country
      * @param {*} country Slug country
@@ -44,7 +66,58 @@ const CoronaAPI = {
                 : `&to=${dayjs().subtract(1, 'days').format('YYYY-MM-DD')}`) : (to ? `?to=${to}` : ''))
 
         return axios.get(url, config)
-    }
+    },
+
+    /**
+     * Get global COVID-19 totals for today, yesterday and two days ago
+     * @param {boolean} yesterday For this parameter you can show yesterday data. Available values : true, false, 1, 0.
+     * @param {boolean} twoDaysAgo Queries data reported two days ago
+     * @param {boolean} allowNull By default, if a value is missing, it is returned as 0. This allows nulls to be returned
+     */
+    summary: (yesterday, twoDaysAgo, allowNull) => axios.get('all?yesterday=' + (yesterday && yesterday === true ? 'true' : 'false') + (twoDaysAgo && twoDaysAgo === true ? '&twoDaysAgo=true' : '') + (allowNull ? '&allowNull=' + allowNull : ''), config_Novel),
+
+    /**
+     * Get COVID-19 totals for a specific country
+     * @param {string} country Country code (ISO2)
+     * @param {boolean} yesterday For this parameter you can show yesterday data
+     * @param {boolean} twoDaysAgo Queries data reported two days ago
+     * @param {boolean} strict Defaults to true. Setting to false gives you the ability to fuzzy search countries. Example Oman vs. rOMANia
+     */
+    specificCountry: (country, yesterday, twoDaysAgo, strict, allowNull) => {
+        const url = 'countries/' + country + '?yesterday=' + (yesterday && yesterday === true ? 'true' : 'false')
+            + (twoDaysAgo && twoDaysAgo === true ? '&twoDayAgo=true' : '')
+            + '&strict=' + ((!strict || strict === true) ? strict : 'false')
+            + (allowNull ? '&allowNull=' + allowNull : '')
+
+        return axios.get(url, config_Novel)
+    },
+
+    /**
+     * Get COVID-19 vaccine doses administered for all countries that have reported rolling out vaccination. Sourced from https://covid.ourworldindata.org/
+     * @param {number | String} lastdays Number of days to return. Use 'all' for the full data set (e.g. 15, all, 24)
+     * Default value : 30
+     * @param {boolean} fullData Flag indicating whether to return timeline data type as simpleVaccineTimeline (false) or fullVaccineTimeline (true).
+     * Default value : false
+     */
+    vaccine: (lastdays, fullData) => axios.get('vaccine/coverage/countries?lastdays=' + (lastdays ? lastdays : '30') + '&fullData=' + (fullData && fullData === true ? fullData : 'false'), config_Novel),
+
+    /**
+     * Get COVID-19 vaccine doses administered for a country that has reported vaccination rollout. Sourced from https://covid.ourworldindata.org/
+     * @param {String} country A valid country name, iso2, iso3
+     * @param {number | String} lastdays Number of days to return. Use 'all' for the full data set (e.g. 15, all, 24)
+     * Default value : 30
+     * @param {boolean} fullData Flag indicating whether to return timeline data type as simpleVaccineTimeline (false) or fullVaccineTimeline (true).
+     * Default value : false
+     */
+    vaccineSpecificByCountry: (country, lastdays, fullData) => axios.get('vaccine/coverage/countries/' + country + '?lastdays=' + (lastdays ? lastdays : '30') + '&fullData=' + (fullData && fullData === true ? fullData : 'false'), config_Novel),
+
+    summaryTotalAsync: () => axios.get(url_oWid_vaccine, {
+
+        params: {
+            t: new Date().getTime(),
+            utm_source: utm_source
+        },
+    })
 }
 
 export default CoronaAPI
