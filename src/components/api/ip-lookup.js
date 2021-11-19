01ede82d68584b65a83c03cd95315d331c06d5be
Truong Hoang <hqtruong27@gmail.com>
2021-11-19 22:09:03 +0700

    new api

--- src/components/api/ip-lookup.js
+++ src/components/api/ip-lookup.js
@@ -1,13 +1,30 @@
 import axios from 'axios'
 
+const baseURL = 'https://api.ipify.org?format=json'
 const IP = {
- lookup: () => {
-  const config = {
-   baseURL: 'https://geolocation-db.com/',
-  }
+    lookup: () => {
+        const config = {
+            baseURL: 'https://geolocation-db.com/',
+        }
 
-  return axios.get('json/', config)
- },
+        return axios.get('json/', config)
+    },
+
+    getLocation: (ip) => {
+        const config = {
+            baseURL: 'https://geolocation-db.com/',
+        }
+
+        return axios.get(`json/${ip}`, config)
+    },
+
+    getCountry: (ip) => {
+        const config = {
+            baseURL: 'https://geolocation-db.com/',
+        }
+
+        return axios.get(`json/${ip}/country`, config)
+    },
 }
 
 export default IP
